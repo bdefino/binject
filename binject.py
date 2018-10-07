14cf724797c6e2ad7acb7c1e945893ca10031605
hiten2 <hiten@riseup.net>
2018-10-07 16:46:32 -0400

    terminology

--- binject.py
+++ binject.py
@@ -8,19 +8,19 @@ class Binject:
     """a mechanism for transmitting data in specific bits of binary"""
     ############needs to control the order in which bits are extracted/injected
     ############should the positions be a tuple of (octet, bit) tuples?
-    def __init__(self, bit_positions = ()):
-        self.bit_positions = bit_positions
+    def __init__(self, positions = ()):
+        self.positions = positions
     
-    def extract(self, packet_generator, trail = False):
-        """generate extracted octets from generated packets"""
+    def extract(self, generator, trail = False):
+        """generate extracted octets from the generated binaries"""
         dest_index = 0
         octet = ittybitty.BitAccess('\0')
         
-        for packet in packet_generator:
-            packet = ittybitty.BitAccess(packet)
+        for binary in generator:
+            binary = ittybitty.BitAccess(binary)
             
-            for bit_position in self.bit_positions:
-                octet[dest_index] = packet[bit_position]
+            for pos in self.positions:
+                octet[dest_index] = binary[pos]
                 dest_index += 1
 
                 if dest_index == 8:
@@ -31,19 +31,27 @@ class Binject:
         if not str(octet) == '\0' and trail: # yield the trailing bits
             yield str(octet)
     
-    def inject(self, packet_generator, src = ''):
-        """inject the octets into generated packets"""
+    def inject(self, generator, src = ''):
+        """
+        inject the octets across the generated binaries
+
+        note that the output binaries will be of the same type as
+        the input ones; their type's constructor must be able to
+        handle a single bytearray argument
+        """
         src = ittybitty.BitAccess(src)
         src_index = 0
         
-        for packet in packet_generator:
+        for binary in generator:
+            _type = type(binary) # the expected type of the binary
+            
             if src: # avoid unnecessary work
-                packet = ittybitty.BitAccess(packet)
+                binary = ittybitty.BitAccess(binary)
                 
-                for bit_position in self.bit_positions:
+                for pos in self.positions:
                     if src_index >= len(src): # early termination
                         break
-                    packet[bit_position] = src[src_index]
+                    binary[pos] = src[src_index]
                     src_index += 1
-                packet = str(packet)
-            yield packet
+                binary = _type(binary)
+            yield binary
