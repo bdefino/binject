b931b1dcb3c2cfc650184e7a732df8ca94142855
hiten2 <hiten@riseup.net>
2018-10-07 18:33:30 -0400

    minor rewrite

--- binject.py
+++ binject.py
@@ -5,29 +5,46 @@ import ittybitty
 __doc__ = "embed bits in binary"
 
 class Binject:
-    """a mechanism for transmitting data in specific bits of binary"""
-    ############needs to control the order in which bits are extracted/injected
-    def __init__(self, positions = ()):
-        self.positions = positions
+    """
+    a mechanism for transmitting data in specific bits of binary
+
+    injected bits appear in little-endian order (lowest order bit first)
+
+    specifying alignment means that an octet's bits cannot be split along
+    multiple binaries
+
+    trail specifies whether to generate bits whose numbers don't
+    make up an entire octet
+    """
     
-    def extract(self, generator, trail = False):
+    def __init__(self, positions = (), align = False, trail = False):
+        assert not align or len(positions) >= 8, "contradictory arguments"
+        self.align = align
+        self.positions = positions
+        self.trail = trail
+    
+    def extract(self, generator):
         """generate extracted octets from the generated binaries"""
         dest_index = 0
         octet = ittybitty.BitAccess('\0')
+        positions = self.positions
+
+        if self.align:
+            positions = positions[:len(positions) / 8 * 8]
         
         for binary in generator:
             binary = ittybitty.BitAccess(binary)
             
-            for pos in self.positions:
+            for pos in positions:
                 octet[dest_index] = binary[pos]
                 dest_index += 1
 
-                if dest_index == 8:
+                if dest_index >= 8:
                     yield str(octet)
                     dest_index = 0
                     octet.clear()
         
-        if not str(octet) == '\0' and trail: # yield the trailing bits
+        if dest_index > 0 and self.trail: # yield the trailing bits
             yield str(octet)
     
     def inject(self, generator, src = ''):
@@ -38,6 +55,10 @@ class Binject:
         the input ones; their type's constructor must be able to
         handle a single bytearray argument
         """
+        positions = self.positions
+
+        if self.align:
+            positions = positions[:len(positions) / 8 * 8]
         src = ittybitty.BitAccess(src)
         src_index = 0
         
@@ -47,7 +68,7 @@ class Binject:
             if src: # avoid unnecessary work
                 binary = ittybitty.BitAccess(binary)
                 
-                for pos in self.positions:
+                for pos in positions:
                     if src_index >= len(src): # early termination
                         break
                     binary[pos] = src[src_index]
